7e63ef78bdd0cf92373bb15ebe04b5108796b056
marcossouz <marss.drive@gmail.com>
2018-09-20 17:44:12 -0300

    req.url

--- myfirst.js
+++ myfirst.js
@@ -3,8 +3,8 @@ var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res){
     res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-    res.write(req.url);
-    res.write("A data atual é: " + dt.myDateTime() + '\n')
+    res.write(req.url + '<br>');
+    res.write("A data atual é: " + dt.myDateTime() + '<br>');
     res.end();
 }).listen(8080);
 console.log('Servidor iniciado em localhost:8080. Ctrl + C para encerrar...');
