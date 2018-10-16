83049689c53a01e04994717f1d33603f5ea068f2
marcossouz <marss.drive@gmail.com>
2018-10-16 19:00:20 +0000

    passando valores para a pagina

--- start-express/index.js
+++ start-express/index.js
@@ -1,43 +1,43 @@
 var express = require('express');
+var handlebars = require('express-handlebars');
 
 var app = express();
 
-//set up handlebars view engine
-var handlebars = require('express3-handlebars').create({defaultLayout: 'main'});
-app.engine('handlebars', handlebars.engine);
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
 
 app.set('port', process.env.PORT || 3000);
 
+var fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",   
+]
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+    res.render('home');
 });
 
 app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('About Meadowlark Travel');
+    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('about', {fortune: randomFortune}); //passando a variaável fortune setada com o valor randomFortune
 });
 
-app.get('/about/msouza', function(req, res){
-    res.type('text/plain');
-    res.send('About Author Marcos Souza');
-});
-
-//custom 404 page
-app.use(function(req, res){
-    res.type('text/plain');
+//404 catch-all handler (middleware)
+app.use(function(req, res, next){
     res.status(404);
-    res.send('404 - Not Found');
+    res.render('404');
 });
 
-//custom 500 page
+//500 error handler (middleware)
 app.use(function(err, req, res, next){
     console.error(err.stack);
-    res.type('text/plain');
     res.status(500);
-    res.send('500 - Server Error');
+    res.render('500');
 });
 
 app.listen(app.get('port'), function(){
